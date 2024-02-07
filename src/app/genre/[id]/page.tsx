10e6f49753dcc4c0512b9c167f28c74646697ca0
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-02-07 19:19:53 -0300

    feat: :sparkles: create a GenrePage

--- src/app/genre/[id]/page.tsx
+++ src/app/genre/[id]/page.tsx
@@ -1,3 +1,6 @@
+import { getDiscoverMovies } from "@/lib/getMovies";
+import { MoviesCarousel } from "@/components";
+
 type Props = {
   params: {
     id: string;
@@ -7,10 +10,15 @@ type Props = {
   };
 };
 
-function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props) {
+  const movies = await getDiscoverMovies(id);
+
   return (
-    <div>
-      GenrePage: {id} and {genre}
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-5 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {genre}</h1>
+      </div>
+      <MoviesCarousel movies={movies} isVertical />
     </div>
   );
 }
