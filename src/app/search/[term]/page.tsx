5b4ef18d2b91c18a7273e7562ba4eb01c96e3f2f
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-02-07 19:15:43 -0300

    feat: :sparkles: create a SearchPage

--- src/app/search/[term]/page.tsx
+++ src/app/search/[term]/page.tsx
@@ -1,17 +1,30 @@
 import { notFound } from "next/navigation";
 
+import { getPopularMovies, getSearchedMovies } from "@/lib/getMovies";
+import { MoviesCarousel } from "@/components";
+
 type Props = {
   params: {
     term: string;
   };
 };
 
-function SearchPage({ params: { term } }: Props) {
+async function SearchPage({ params: { term } }: Props) {
   if (!term) notFound();
 
   const decodeTerm = decodeURI(term);
+  const movies = await getSearchedMovies(decodeTerm);
+  const popularMovies = await getPopularMovies();
 
-  return <div>SearchPage: {decodeTerm}</div>;
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex flex-col space-y-4 mt-32 xl:mt-42">
+        <h1 className="text-6xl font-bold px-10">Results for {decodeTerm}</h1>
+        <MoviesCarousel title="Movies" movies={movies} isVertical />
+        <MoviesCarousel title="You may also like" movies={popularMovies} />
+      </div>
+    </div>
+  );
 }
 
 export default SearchPage;
