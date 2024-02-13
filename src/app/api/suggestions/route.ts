1b603171ca10cdca5dfbdc88e0e7de99c15b89c9
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2024-02-13 16:13:47 -0300

    fix: :bug: erro in suggestions api function

--- src/app/api/suggestions/route.ts
+++ src/app/api/suggestions/route.ts
@@ -1,12 +1,13 @@
-export async function getAISuggestion(request: Request) {
+export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const term = searchParams.get("term");
+
   const res = await fetch(
-    `https://disney-clone-ic.azurewebsites.net/api/getaisuggestion?term=${term}`,
+    `https://disney2clone.azurewebsites.net/api/getaisuggestion?term=${term}`,
     {
       method: "GET",
       next: {
-        revalidate: 60 * 60 * 24, // 24 hours
+        revalidate: 60 * 60 * 24,
       },
     }
   );
